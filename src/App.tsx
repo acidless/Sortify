e8a13565133762dfb0f2a5b55e2c22657083b490
Oleg <Oleg@DESKTOP-B5TOANR>
2025-08-22 11:37:05 +0300

    refactor: better routing layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import './App.css'
-import SideMenu from "./components/SideMenu.tsx";
 import {BrowserRouter, Routes, Route} from "react-router";
 import BubbleSort from "./components/sorting/BubbleSort.tsx";
 import InsertionSort from "./components/sorting/InsertionSort.tsx";
@@ -11,38 +10,28 @@ import QuickSort from "./components/sorting/QuickSort.tsx";
 import BFS from "./components/graphs/BFS.tsx";
 import DFS from "./components/graphs/DFS.tsx";
 import Home from "./components/Home.tsx";
-import {useState} from "react";
-import { TheoryContext } from './TheoryContext.ts';
 import Dijkstra from "./components/graphs/Dijkstra.tsx";
-import Theory from "./components/Theory.tsx";
+import Layout from './components/Layout.tsx';
 
 function App() {
-    const [theory, setTheory] = useState(null);
-
     return (
-        <div className="flex h-max min-h-full overflow-x-hidden">
-            <TheoryContext.Provider value={{theory, setTheory}}>
-                <BrowserRouter>
-                    <SideMenu></SideMenu>
-                    <main className="p-4 md:p-10 w-full h-max min-h-full flex flex-col overflow-x-hidden relative">
-                        <Routes>
-                            <Route path="/" element={<Home/>}/>
-                            <Route path="/bubble-sort" element={<BubbleSort/>}/>
-                            <Route path="/insertion-sort" element={<InsertionSort/>}/>
-                            <Route path="/merge-sort" element={<MergeSort/>}/>
-                            <Route path="/quick-sort" element={<QuickSort/>}/>
-                            <Route path="/bfs" element={<BFS/>}/>
-                            <Route path="/dfs" element={<DFS/>}/>
-                            <Route path="/dijkstra" element={<Dijkstra/>}/>
-                            <Route path="/bst-search" element={<BSTSearch/>}/>
-                            <Route path="/bst-insert" element={<BSTInsert/>}/>
-                            <Route path="/bst-delete" element={<BSTDelete/>}/>
-                        </Routes>
-                        <Theory></Theory>
-                    </main>
-                </BrowserRouter>
-            </TheoryContext.Provider>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout/>}>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/bubble-sort" element={<BubbleSort/>}/>
+                    <Route path="/insertion-sort" element={<InsertionSort/>}/>
+                    <Route path="/merge-sort" element={<MergeSort/>}/>
+                    <Route path="/quick-sort" element={<QuickSort/>}/>
+                    <Route path="/bfs" element={<BFS/>}/>
+                    <Route path="/dfs" element={<DFS/>}/>
+                    <Route path="/dijkstra" element={<Dijkstra/>}/>
+                    <Route path="/bst-search" element={<BSTSearch/>}/>
+                    <Route path="/bst-insert" element={<BSTInsert/>}/>
+                    <Route path="/bst-delete" element={<BSTDelete/>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
     )
 }
 
