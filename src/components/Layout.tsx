01ed3630a91fc30a40e4708e22e18eb2292bd8f9
Oleg <kukunov2004@gmail.com>
2025-08-22 11:37:05 +0300

    refactor: better routing layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,21 @@
+import {TheoryContext} from "../TheoryContext.ts";
+import {Outlet} from "react-router";
+import SideMenu from "./SideMenu.tsx";
+import Theory from "./Theory.tsx";
+import {useState} from "react";
+
+function Layout() {
+    const [theory, setTheory] = useState(null);
+
+    return <div className="flex h-max min-h-full overflow-x-hidden">
+        <TheoryContext.Provider value={{theory, setTheory}}>
+            <SideMenu></SideMenu>
+            <main className="p-4 md:p-10 w-full h-max min-h-full flex flex-col overflow-x-hidden relative">
+                <Outlet></Outlet>
+                <Theory></Theory>
+            </main>
+        </TheoryContext.Provider>
+    </div>
+}
+
+export default Layout;
