8ca14c2a170ea45ef2d63f8ba1d1337806d98d1c
Oleg <Oleg@HOME-PC>
2025-09-10 00:05:54 +0300

    fix(PopupText.test.tsx): id should be string

--- src/components/tests/PopupText.test.tsx
+++ src/components/tests/PopupText.test.tsx
@@ -3,7 +3,7 @@ import {render} from "@testing-library/react";
 
 describe("PopupText", () => {
    it("should contain given text", () => {
-        const popupText = render(<PopupText id={1} text="Hello World" />);
+        const popupText = render(<PopupText id="1" text="Hello World" />);
         expect(popupText.getByText("Hello World")).toBeInTheDocument();
    });
 });
